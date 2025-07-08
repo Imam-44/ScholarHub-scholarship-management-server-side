805e856ee293a6af93f973acb6d2a65320dee8fe
Imam Hasan <imamets3@gmail.com>
2025-07-08 10:33:07 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// server/index.js
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port  = process.env.PORT ||  5000; // server port
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
