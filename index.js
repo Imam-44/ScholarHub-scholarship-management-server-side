b9b06e21949dc3826331d4b362e08dbb115d09da
Imam Hasan <imamets3@gmail.com>
2025-07-09 17:00:03 +0600

    add mongobd and jwt authentication

--- index.js
+++ index.js
@@ -3,14 +3,92 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port  = process.env.PORT ||  5000; // server port
+const port = process.env.PORT || 5000; // server port
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
-app.use(cors());
-app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Server is running!');
+
+const corsOptions = {
+  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(cookieParser())
+
+const varifyToken = async (req, res, next) => {
+  const token = req.cookies?.token
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' })
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: 'usauthorized access' })
+    }
+    req.user = decoded
+    next()
+  })
+}
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
+
+async function run() {
+  try {
+
+    //jwt
+    app.post('/jwt', async (req, res) => {
+      const email = req.body
+      const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '365d',
+      })
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      }).send({ success: true })
+    })
+
+    //logout jwt 
+    app.get('/logout', async (req, res) => {
+      try {
+        res.clearCookie('token', {
+          maxAge: 0,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        }).send({ success: true })
+      } catch (err) {
+        res.status(500).send(err)
+      }
+    })
+
+
+
+    app.get('/', (req, res) => {
+      res.send('Server is running!');
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
